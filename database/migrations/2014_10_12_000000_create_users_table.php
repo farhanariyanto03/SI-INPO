35979632c74faaf40e5965ee0da2764a8e122693
farhanariyanto03 <ariyantofarhan03@gmail.com>
2024-05-15 16:48:24 +0700

    table user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('nama', 30);
+            $table->string('jenis_kelamin', 20);
+            $table->string('email', 30)->unique();
+            $table->string('password', 8);
+            $table->char('no_hp', 13);
+            $table->string('role', 5);
             $table->rememberToken();
             $table->timestamps();
         });
